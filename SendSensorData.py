2437b1351fb2ec0ef5751ea2de4c5ac42b2809a4
Leonard Lee <archerleonard@gmail.com>
2017-12-04 02:40:49 -0800

    add functions for temperature, humidity, and ip

--- SendSensorData.py
+++ SendSensorData.py
@@ -3,6 +3,8 @@ import paho.mqtt.client as mqtt
 import paho.mqtt.publish as publish
 import time, json, ssl
 from time import sleep
+import netifaces as ni
+import Adafruit_DHT
 
 connflag = False
 
@@ -18,6 +20,37 @@ def getNow():
     localtime = time.localtime()
     return (time.strftime("%Y%m%d_%H%M%S", localtime))
 
+def temphumid():
+    sensor_args = {'11': Adafruit_DHT.DHT11,
+                   '22': Adafruit_DHT.DHT22,
+                   '2302': Adafruit_DHT.AM2302}
+    sensor = sensor_args['11']
+    pin = 17  ##BCM PIN PORT
+
+    # Try to grab a sensor reading.  Use the read_retry method which will retry up
+    # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
+    humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+
+    # Un-comment the line below to convert the temperature to Fahrenheit.
+    temperature = temperature * 9 / 5.0 + 32
+
+    # Note that sometimes you won't get a reading and
+    # the results will be null (because Linux can't
+    # guarantee the timing of calls to read the sensor).
+    # If this happens try again!
+    if humidity is not None and temperature is not None:
+        '{:0.2f}'.format(3.141592653589793)
+        strTemp = '{:0.2f}*F'.format(temperature)
+        strHumidity = '{:0.2f}%'.format(humidity)
+        return (strTemp, strHumidity)
+    else:
+        return ('-9999.99', '-9999.99')
+
+def getIP():
+    # get the private IP
+    private_ip = ni.ifaddresses('wlan0')[ni.AF_INET][0]['addr']
+    print private_ip
+
 client = mqtt.Client(client_id='aws_temp_humid_client', protocol=mqtt.MQTTv311)
 client.on_connect = on_connect
 client.on_message = on_message
@@ -28,7 +61,10 @@ client.tls_set(ca_certs='certs/root.ca.pem',
                ciphers=None)
 client.tls_insecure_set(True)
 client.connect('agwj5stucs72x.iot.us-west-2.amazonaws.com', 8883, 60)
-msg = dict(id = getNow())
+temp, humid = temphumid()
+ip = getIP()
+msg = dict(temperature = temp, humidity = humid, ip = ip, timestamp = getNow())
+print(msg)
 client.loop_start()
 
 while True:
