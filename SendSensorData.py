fc8e629e57a89d284a887c0a3b97da17093c5ff7
Leonard-Lee <archerleonard@gmail.com>
2017-12-04 02:54:52 -0800

    client tls setup one line

--- SendSensorData.py
+++ SendSensorData.py
@@ -54,11 +54,7 @@ def getIP():
 client = mqtt.Client(client_id='aws_temp_humid_client', protocol=mqtt.MQTTv311)
 client.on_connect = on_connect
 client.on_message = on_message
-client.tls_set(ca_certs='certs/root.ca.pem',
-               keyfile='certs/fcb5dd2aea-private.pem.key',
-               certfile='certs/fcb5dd2aea-certificate.pem.crt',
-               tls_version=ssl.PROTOCOL_TLSv1_2,
-               ciphers=None)
+client.tls_set(ca_certs='certs/root.ca.pem', keyfile='certs/fcb5dd2aea-private.pem.key', certfile='certs/fcb5dd2aea-certificate.pem.crt', tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
 client.tls_insecure_set(True)
 client.connect('agwj5stucs72x.iot.us-west-2.amazonaws.com', 8883, 60)
 temp, humid = temphumid()
